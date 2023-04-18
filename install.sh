ce3558966737f58b816edf2dcfea8fc8ac7b33bb
João Victor Viana <joao.duarte@smplaces.com.br>
2023-04-17 22:13:01 -0300

    chore(core): configuração do docker-compose

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+cat 'Iniciando instalação...'
+
+cp .env-example .env
+
 git submodule init
 git submodule update --remote --merge
+
+cat 'Instalando Api...'
 
 cd ./projects/coopers-api
 yarn
 
+cat 'Instalando Front...'
 cd ../coopers-front
 yarn
