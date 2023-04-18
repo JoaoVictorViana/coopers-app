cae11ab634da0f7eb49a8171e29bbb759a7cfeaf
João Victor Viana <joao.duarte@smplaces.com.br>
2023-04-17 22:19:13 -0300

    fix(core): substituicao do cat por echo

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cat 'Iniciando instalação...'
+echo 'Iniciando instalação...'
 
 cp ./.env-example ./.env
 
 git submodule init
 git submodule update --remote --merge
 
-cat 'Instalando Api...'
+echo 'Instalando Api...'
 
 cd ./projects/coopers-api
 yarn
 
-cat 'Instalando Front...'
+echo 'Instalando Front...'
 cd ../coopers-front
 yarn
